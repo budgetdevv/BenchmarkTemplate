07f1ff7eef204f5d8287c07e8a3f002976197137
TrumpMcDonaldz <budgetdevv@gmail.com>
2022-08-23 03:22:48 +0800

    First commit

---
+++ BenchmarkTemplate/Bench.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[DisassemblyDiagnoser(exportDiff: true, exportCombinedDisassemblyReport: true)]
+public class Bench
+{
+    [ModuleInitializer]
+    public static void RunCctor()
+    {
+        RuntimeHelpers.RunClassConstructor(typeof(Bench).TypeHandle);
+    }
+
+    static Bench()
+    {
+        
+    }
+    
+    [Benchmark]
+    public void Foo()
+    {
+        
+    }
+}
