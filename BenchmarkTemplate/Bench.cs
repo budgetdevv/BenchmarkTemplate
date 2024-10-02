de9cb19b645ce6fe3da6f970ac94d72343101970
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-10-02 14:03:07 +0800

    Modernize template

--- BenchmarkTemplate/Bench.cs
+++ BenchmarkTemplate/Bench.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
 using BenchmarkDotNet.Order;
+#if OS_IS_WINDOWS
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+#endif
 
 namespace BenchmarkTemplate
 {
-    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    #if OS_IS_WINDOWS
+    [NativeMemoryProfiler]
+    [InliningDiagnoser(logFailuresOnly: true, filterByNamespace: true)]
     [DisassemblyDiagnoser(exportDiff: true, exportCombinedDisassemblyReport: true)]
+    #endif
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class Bench
     {
         [ModuleInitializer]
--- BenchmarkTemplate/Program.cs
+++ BenchmarkTemplate/Program.cs
@@ -1,14 +1,25 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Running;
 
 namespace BenchmarkTemplate
 {
-    internal class Program
+    internal static class Program
     {
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
         private static void Main(string[] args)
         {
             BenchmarkRunner.Run<Bench>();
 
-            while (true) { }
+            while (true)
+            {
+                Console.WriteLine("\nDone! Press X to exit.");
+                
+                if (Console.ReadKey().Key == ConsoleKey.X)
+                {
+                    break;
+                }
+            }
         }
     }
 }
