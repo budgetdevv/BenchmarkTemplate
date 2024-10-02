2fde85d7e7049c1e15c7681cf741aef767f9d900
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-10-02 14:03:07 +0800

    Modernize template

--- BenchmarkTemplate/Bench.cs
+++ BenchmarkTemplate/Bench.cs
@@ -1,11 +1,19 @@
 using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
+#if OS_IS_WINDOWS
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+#endif
 
 namespace BenchmarkTemplate
 {
-    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    #if OS_IS_WINDOWS
+    [NativeMemoryProfiler]
+    [InliningDiagnoser(logFailuresOnly: true, filterByNamespace: true)]
     [DisassemblyDiagnoser(exportDiff: true, exportCombinedDisassemblyReport: true)]
+    #endif
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class Bench
     {
         [ModuleInitializer]
